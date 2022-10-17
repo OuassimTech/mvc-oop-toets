4c54daa49f161d029b6908646f435fd78697ab9b
Ouassimtech <ouassimtech@gmail.com>
2022-10-17 08:56:38 +0200

    Finished RichPerson Modal

--- app/models/RichPerson.php
+++ app/models/RichPerson.php
@@ -1,5 +1,5 @@
 <?php
-  class Country {
+  class RichPerson {
     private $db;
 
     public function __construct() {
@@ -12,6 +12,12 @@
       $result = $this->db->resultSet();
 
       return $result;
+    }
+
+    public function deleteRichPerson($Id) {
+      $this->db->query("DELETE FROM RichPeople WHERE Id = :Id");
+      $this->db->bind("Id", $Id, PDO::PARAM_INT);
+      return $this->db->execute();
     }
   }
 
