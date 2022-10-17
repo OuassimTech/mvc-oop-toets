80f13c4f840b4ff3eacf8e044cc530984cef5336
Ouassimtech <ouassimtech@gmail.com>
2022-10-17 08:41:08 +0200

    Changed URLROOT and database name

--- app/config/config.php
+++ app/config/config.php
@@ -2,11 +2,11 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'mvc');
+define('DB_NAME', 'mvc-oop-toets');
 
 define('APPROOT', dirname(dirname(__FILE__))) ;
 
-define('URLROOT', 'http://www.mvcopdracht1.nl/');
+define('URLROOT', 'http://www.mvc-oop-toets.com/');
 
 define('SITENAME', 'MVC Framework reference');
 ?>
